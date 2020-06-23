f5df888721ed7d0d52aff170cab77b6cdf469953
kucingorenn <65481065+kucingorenn@users.noreply.github.com>
2020-06-24 01:00:54 +0700

    mouse tidak update, semua yang dilayar terhapus jika ada 1 downstate

--- GrafkomG-SA2020-Tugas3/Source.cpp
+++ GrafkomG-SA2020-Tugas3/Source.cpp
@@ -1,36 +1,45 @@
 #include <iostream>
 #include <GL/freeglut.h>
 
+bool* keystate = new bool[256];
+//unsigned char keystate[256];
+
 void display(void) {
 }
 
 void mouse(int button, int state, int x, int y) {
-	if(state == 1)
+	if (state == 0);
 	system("cls");
-	std::cout<< state << "Klik kiri | X : " << x << " Y : " << y << std::endl;
-}
-
-void keydown(unsigned char key, int x, int y) {
-	std::cout << key << " ";
+	std::cout << "Klik Mouse | X : " << x << " Y : " << y << std::endl;
 }
 
 void keyup(unsigned char key, int x, int y) {
+	keystate[key] = false;
+	if (keystate[key] == false) {
+		system("cls");
+		
+	}
 	
-	std::cout << key ;
-	system("cls");
+}
+
+void keydown(unsigned char key, int x, int y) {
+	keystate[key] = true;
+	std::cout << key << " ";
+	glutKeyboardUpFunc(keyup);
+	
 }
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_MULTISAMPLE);
 	glutInitWindowPosition(0, 0);
-	glutInitWindowSize(100, 100);
+	glutInitWindowSize(50, 50);
 	glutCreateWindow(argv[0]);
 	glutDisplayFunc(display);
+	glutSetKeyRepeat(GLUT_KEY_REPEAT_OFF);
 
 	glutMouseFunc(mouse);
 	glutKeyboardFunc(keydown);
-	glutKeyboardUpFunc(keyup);
 	
 
 	glClearColor(0, 0, 0, 1);
